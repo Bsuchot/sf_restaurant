fe712d8fe1604e14d874f138bf8f4ad7f0a1d048
Bsuchot <bastien.suchot@gmail.com>
2026-08-13 14:13:59 +0200

    maj composer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 class HomeController
 {
+    #[Route('/', name: 'home')]
+    public function home() : Response
+    {
+        return new Response('Bienvenue sur la page accueil');
+    }
 
 }
